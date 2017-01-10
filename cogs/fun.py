7f8aca3915d6a03158e917843e85b5787bed5429
jacobcheatley <jacobcheatley@gmail.com>
2017-01-11 11:58:41 +1300

    Upside down text

--- cogs/fun.py
+++ cogs/fun.py
@@ -273,6 +273,63 @@ class Fun:
         """STOP :clap: APPROPRIATING :clap: BLACK :clap: CULTURE"""
         await self.bot.say(text.replace(' ', '\U0001f44f') + random.randint(1, 5) * '\U0001f44f')
 
+    @text.command(aliases=['u'])
+    async def upsidedown(self, *, text: str):
+        """uʍop ǝpısdn"""
+        replacements = {
+            'a': '\u0250',
+            'b': 'q',
+            'c': '\u0254',
+            'd': 'p',
+            'e': '\u01DD',
+            'f': '\u025F',
+            'g': 'b',
+            'h': '\u0265',
+            'i': '\u0131',
+            'j': '\u0638',
+            'k': '\u029E',
+            'l': '\u05DF',
+            'm': '\u026F',
+            'n': 'u',
+            'o': 'o',
+            'p': 'd',
+            'q': 'b',
+            'r': '\u0279',
+            's': 's',
+            't': '\u0287',
+            'u': 'n',
+            'v': '\u028C',
+            'w': '\u028D',
+            'x': 'x',
+            'y': '\u028E',
+            'z': 'z',
+            '[': ']',
+            ']': '[',
+            '(': ')',
+            ')': '(',
+            '{': '}',
+            '}': '{',
+            '<': '>',
+            '>': '<',
+            '?': '\u00BF',
+            '\u00BF': '?',
+            '\'': ',',
+            ',': '\'',
+            '!': '\u00A1',
+            '.': '\u02D9',
+            '_': '\u203E',
+            ';': '\u061B',
+            '9': '6',
+            '6': '9',
+        }
+
+        def get_replacement(c: str):
+            if c in replacements:
+                return replacements[c]
+            return c
+
+        await self.bot.say(''.join(get_replacement(c.lower()) for c in reversed(text)))
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))
